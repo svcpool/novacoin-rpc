9aba6eddc30f3bd8bcace02b2d1fa13be0579710
Anton Morev <shikto@mail.ru>
2018-01-30 12:42:09 +0300

    wip

---
+++ connector/RpcConnector.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Svcpool\N;
+
+use common\components\crypto\rpc\client\RpcClient;
+
+/**
+ * Class RpcConnector
+ * @package common\components\crypto\rpc
+ */
+class RpcConnector
+{
+    /**
+     * @var RpcClient
+     */
+    protected $_client;
+
+    /**
+     * RpcConnector constructor.
+     * @param RpcClient $_client
+     */
+    public function __construct(RpcClient $_client)
+    {
+        $this->_client = $_client;
+    }
+
+    /**
+     * @return RpcClient
+     */
+    public function getClient()
+    {
+        return $this->_client;
+    }
+
+    public function scanInput($txid)
+    {
+        return $this->_client->scaninput(['txid' => $txid]);
+    }
+
+    public function listUnspent()
+    {
+        return $this->_client->callMethod('listunspent');
+    }
+
+    public function getBalance($account = NULL)
+    {
+        return $this->_client->getBalance($account);
+    }
+
+    public function getAccountForAddress($address)
+    {
+        return $this->_client->getaccount($address);
+    }
+
+    public function getTransationInfo($txId)
+    {
+        return $this->_client->gettransaction($txId);
+    }
+
+
+    public function createAddress($account = NULL)
+    {
+        $result = $this->_client->getnewaddress($account);
+
+        return $result;
+    }
+
+    public function getTransactions($count = 10, $offset = 0)
+    {
+        return $this->_client->listtransactions('*', $count, $offset);
+    }
+
+
+}
---
+++ connector/exceptions/RpcClientException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zvinger
+ * Date: 15.11.17
+ * Time: 8:07
+ */
+
+namespace common\components\crypto\rpc\exceptions;
+
+use yii\base\Exception;
+
+class RpcClientException extends Exception
+{
+
+}
