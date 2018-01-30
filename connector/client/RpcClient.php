c205ab43d10b97630322ced3330afea977985677
Anton Morev <shikto@mail.ru>
2018-01-30 18:14:21 +0300

    wip

---
+++ connector/client/RpcClient.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zvinger
+ * Date: 25.09.17
+ * Time: 9:15
+ */
+
+namespace Svcpool\NovacoinRpc\connector\client;
+
+use Svcpool\NovacoinRpc\connector\exceptions\RpcClientException;
+use liamwli\PHPBitcoin\BitCoin;
+
+/**
+ * Class RpcClient
+ * @package Svcpool\NovacoinRpc\connector\client
+ * @method getBalance($account = NULL)
+ * @method getnewaddress($account = NULL)
+ * @method scaninput($params)
+ * @method gettransaction($txid)
+ * @method getaccount($address)
+ * @method listtransactions($account, $count, $from)
+ */
+class RpcClient extends BitCoin
+{
+    public function callMethod($methodName, array $parameters = array())
+    {
+        $result = parent::callMethod($methodName, $parameters);
+        $error = $this->getError();
+        if ($result === FALSE && $error) {
+            throw new RpcClientException($error);
+        }
+
+        return $result;
+    }
+
+}
---
+++ connector/exceptions/RpcClientException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zvinger
+ * Date: 15.11.17
+ * Time: 8:07
+ */
+
+namespace Svcpool\NovacoinRpc\connector\exceptions;
+
+use yii\base\Exception;
+
+class RpcClientException extends Exception
+{
+
+}
